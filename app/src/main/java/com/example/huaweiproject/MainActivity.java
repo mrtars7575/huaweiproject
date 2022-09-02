068799dc26e179ad8d764d750de56f576850753d
Murat Arslan <mrtars2000@gmail.com>
2022-09-02 15:38:56 +0300

    current location with location kit

--- app/src/main/java/com/example/huaweiproject/MainActivity.java
+++ app/src/main/java/com/example/huaweiproject/MainActivity.java
@@ -1,10 +1,50 @@
 package com.example.huaweiproject;
 
+import static java.util.Locale.SIMPLIFIED_CHINESE;
+
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
+import com.google.android.material.snackbar.Snackbar;
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.common.ResolvableApiException;
+import com.huawei.hms.location.FusedLocationProviderClient;
+import com.huawei.hms.location.LocationCallback;
+import com.huawei.hms.location.LocationRequest;
+import com.huawei.hms.location.LocationResult;
+import com.huawei.hms.location.LocationServices;
+import com.huawei.hms.location.LocationSettingsRequest;
+import com.huawei.hms.location.LocationSettingsResponse;
+import com.huawei.hms.location.LocationSettingsStates;
+import com.huawei.hms.location.LocationSettingsStatusCodes;
+import com.huawei.hms.location.SettingsClient;
 import com.huawei.hms.maps.CameraUpdateFactory;
 import com.huawei.hms.maps.HuaweiMap;
 import com.huawei.hms.maps.MapView;
@@ -13,43 +53,246 @@ import com.huawei.hms.maps.OnMapReadyCallback;
 import com.huawei.hms.maps.model.LatLng;
 import com.huawei.hms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback  {
+
+    private static final String TAG = "MainActivity";
+    private static final int GET_DETAIL_ADDRESS = 0;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
     private MapView mapView;
     private HuaweiMap hMap;
-    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private String mAddress;
+    private LocationCallback mLocationCallback;
+    private SettingsClient mSettingsClient;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mProviderClient;
+
+
+    @SuppressLint("HandlerLeak")
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NotNull Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == GET_DETAIL_ADDRESS) {
+                mAddress = msg.obj.toString();
+               System.out.println("address  : "  + mAddress);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MapsInitializer.initialize(this);
         MapsInitializer.setApiKey("DAEDADBfDgUVHydgn/rZGmpU7Sw4nGraSNn1rA4rfcYD59JG1XxAztkZCvdV4AnplUsEKyu8UslA9+MYc6zxQAl9t29QSYs97hfhgQ==");
+
         setContentView(R.layout.activity_main);
 
-        
-
-
         mapView = findViewById(R.id.mapview_mapviewdemo);
+
+
+        initLocationRequest();
+        requestLocationUpdates();
+
+
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey");
         }
         mapView.onCreate(mapViewBundle);
         mapView.getMapAsync(this);
+
+
+
     }
+
+    private void initLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        //Sets the interval for location update (unit: Millisecond)
+        mLocationRequest.setInterval(10000);
+        //Sets the priority
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private LocationCallback initLocationCallback() {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                if (locationResult != null) {
+                    List<Location> locations = locationResult.getLocations();
+                    if (null != locations && locations.size() > 0) {
+                        Location location = locations.get(0);
+                        final double  longitude = location.getLongitude();
+                        final double latitude = location.getLatitude();
+
+
+
+                        System.out.println("longitude :" + longitude + "latittude  : " + latitude);
+                        // Enable the subthread to invoke the reverse geocoding capability to obtain location information.
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    // Reverse geocoding address
+                                    final Geocoder geocoder = new Geocoder(MainActivity.this, SIMPLIFIED_CHINESE);
+                                    List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+                                    // After the address information is updated successfully, use the handler to update the UI.
+                                    if (null != addresses && addresses.size() > 0) {
+                                        for (Address address : addresses) {
+                                            Message msg = new Message();
+                                            msg.what = GET_DETAIL_ADDRESS;
+                                            msg.obj = addresses.get(0).getFeatureName();
+                                            handler.sendMessage(msg);
+
+                                            runOnUiThread(new Runnable() {
+                                                @Override
+                                                public void run() {
+
+
+
+                                                    LatLng latLng = new LatLng(latitude,longitude);
+                                                    hMap.addMarker(new MarkerOptions()
+                                                            .position(latLng)
+                                                            .title("Murat"));
+
+                                                    hMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                                                }
+                                            });
+
+                                        }
+                                    }
+                                } catch (Exception e) {
+                                    System.out.println("reverseGeocode wrong ");
+                                }
+                            }
+                        }).start();
+
+
+                    }
+                }
+            }
+        };
+        return mLocationCallback;
+    }
+
+    private void removeLocationUpdates() {
+        if (null != mProviderClient && null != mLocationCallback) {
+            mProviderClient.removeLocationUpdates(mLocationCallback).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(Exception e) {
+                }
+            });
+        }
+    }
+
+    private void requestLocationUpdates() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        mLocationRequest = new LocationRequest();
+        builder.addLocationRequest(mLocationRequest);
+        LocationSettingsRequest locationSettingsRequest = builder.build();
+        mProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+        // Check the device location settings.
+        mSettingsClient.checkLocationSettings(locationSettingsRequest)
+        .addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // Set the location conditions to be met and then initiate a location request.
+                mProviderClient.requestLocationUpdates(mLocationRequest, initLocationCallback(), Looper.getMainLooper())
+                    // Listening callback for successful location update request interface
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            System.out.println("The callback is executed successfully.");
+                        }
+                    });
+            }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            int statusCode = ((ApiException) e).getStatusCode();
+                            switch (statusCode) {
+                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                    try {
+                                        ResolvableApiException rae = (ResolvableApiException) e;
+                                        rae.startResolutionForResult(MainActivity.this, 0);
+                                    } catch (IntentSender.SendIntentException sie) {
+                                        System.out.println("sie=" + sie.getMessage());
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+        );
+    }
+
+    private void requestPermission(){
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                String[] strings =
+                        {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+                ActivityCompat.requestPermissions(this, strings, 1);
+            }
+        } else {
+            if (ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this,
+                    "android.permission.ACCESS_BACKGROUND_LOCATION") != PackageManager.PERMISSION_GRANTED){
+                String[] strings = {android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        "android.permission.ACCESS_BACKGROUND_LOCATION"};
+                ActivityCompat.requestPermissions(this, strings, 2);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                System.out.println("onRequestPermissionsResult: apply LOCATION PERMISSION successful");
+            } else {
+                System.out.println("onRequestPermissionsResult: apply LOCATION PERMISSSION  failed");
+            }
+        }
+
+        if (requestCode == 2) {
+            if (grantResults.length > 2 && grantResults[2] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                System.out.println("onRequestPermissionsResult: apply ACCESS_BACKGROUND_LOCATION successful");
+            } else {
+                System.out.println("onRequestPermissionsResult: apply ACCESS_BACKGROUND_LOCATION  failed");
+            }
+        }
+    }
+
 
     @Override
     public void onMapReady(HuaweiMap huaweiMap) {
         hMap = huaweiMap;
         hMap.setMapType(HuaweiMap.MAP_TYPE_NORMAL);
-        LatLng sydney = new LatLng(-34,151);
-        hMap.addMarker(new MarkerOptions()
-                .position(sydney)
-                .title("Marker is sydney"));
 
-        hMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        requestPermission();
 
     }
+
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
